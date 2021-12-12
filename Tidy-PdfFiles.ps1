9fc18c06f82e609a5bfe673e88bf2d098452c0c3
xMekko <45107753+xMekko@users.noreply.github.com>
2021-12-12 12:11:10 +0100

    Add files via upload

---
+++ Tidy-PdfFiles.ps1
@@ -0,0 +1,18 @@
+﻿
+cd .\
+if ( -not ( Test-Path .\PDFy -PathType Container ) ) {
+    New-Item -Name PDFy -ItemType Directory
+}
+foreach ($plik in Get-ChildItem -Path .\* -Include *.pdf) {
+    if ( -not ( Test-Path .\PDFy\$plik.Name -PathType Leaf ) ) {
+        Move-Item $plik.Name -Destination .\PDFy\
+    }
+}
+cd .\PDFy
+foreach ($plik in Get-ChildItem -Path .\*) {
+    $utworzono = $plik.CreationTime.toString("dd.MM.yyyy")
+    if ( -not (  Test-Path .\$utworzono -PathType Container ) ) {
+        New-Item -Name $utworzono -ItemType Directory
+    }
+    Move-Item -Path $plik -Destination .\$utworzono\
+}
